25cd590d2a4ff133be17b298ae4ac24e906e56f7
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-09-09 16:54:14 +0300

    added social media card

--- other_methods.js
+++ other_methods.js
@@ -12,12 +12,13 @@ async function sendSocialMedia(client, to){
     /**
      * TESTING NEW CARDS
      * 
-     */
-
-    const response = await createTwilioCard();
+     * const response = await createTwilioCard();
     const sid = response.sid;
 
     console.log(sid);
+     */
+
+    const sid = process.env.SOCIAL_MEDIA_CONTENT;
 
     try {
         await client.messages.create({
