5f0b98c74c373bfda18e1cf2ff1c921c351def0d
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-09-09 15:54:59 +0300

    fix bug

--- component_builder/list_builder.js
+++ component_builder/list_builder.js
@@ -9,50 +9,61 @@ const { Client, Colors, EmbedBuilder } = require("discord.js");
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 
-/**
- * @param {Twilio} client
- * 
- */
-async function createTwilioListPicker(client) {
+const axios = require('axios');
+
+async function createTwilioListPicker() {
+    const url = 'https://content.twilio.com/v1/Content';
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+
     try {
-        // Define the list picker items
-        const options = [
-            {
-                id: 'general_info',
-                item: 'Genel Bilgi🌞',
-                description: 'Bizim kim olduğumuzu ve bu süreçte neler amaçladığımızı öğren.'
-            },
-            {
-                id: 'current_activities',
-                item: 'Aktif Etkinlikler✨',
-                description: 'Devam eden etkinlikleri göreceksin, bunlardan bazılarına başvurabilirsin'
-            }
-        ];
-    
-
-        // Define the list picker body
-        const listPickerBody = "Owl Air Flash Sale! Hurry! Sale ends on {{1}}!";
-
-        // Construct the content request object
-        const contentCreateRequest = {
-            friendlyName: "owl_sale_list",
+        // Make the POST request
+        const response = await axios.post(url, {
+            friendly_name: "owl_air_list",
             language: "en",
-            variables: { "1": "end_date" },
+            variables: {
+                "1": "end_date"
+            },
             types: {
-                twilio_list_picker: {
-                    body: listPickerBody,
-                    items: options
+                "twilio/list-picker": {
+                    body: "Owl Air Flash Sale! Hurry! Sale ends on {{1}}!",
+                    button: "Select a destination",
+                    items: [
+                        {
+                            item: "SFO to NYC for $299",
+                            description: "Owl Air Flight 1337 to LGA",
+                            id: "SFO1337"
+                        },
+                        {
+                            item: "OAK to Denver for $149",
+                            description: "Owl Air Flight 5280 to DEN",
+                            id: "OAK5280"
+                        },
+                        {
+                            item: "LAX to Chicago for $199",
+                            description: "Owl Air Flight 96 to ORD",
+                            id: "LAX96"
+                        }
+                    ]
+                },
+                "twilio/text": {
+                    body: "We have flights to the following destinations: (1) SFO, (2) OAK, (3) LAX. Hurry! Sale ends on {{1}}!"
                 }
             }
-        };
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            auth: {
+                username: accountSid,
+                password: authToken
+            }
+        });
 
-        // Use the Twilio Content API to create the content template
-        const contentTemplate = await client.content.v1.create(contentCreateRequest);
-        console.log(`Created Twilio Content Template SID: ${contentTemplate.sid}`);
+        console.log('Response:', response.data);
     } catch (error) {
-        console.error("Error creating content template:", error);
+        console.error('Error creating content template:', error.response ? error.response.data : error.message);
     }
 }
 
-// Export the function for use in other parts of your application
 module.exports = createTwilioListPicker;
