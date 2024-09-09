ec7eaebdad711b02988bd3477009d52a6ccafe10
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-09-09 21:26:36 +0300

    trivia option list added

--- main_features/trivia.js
+++ main_features/trivia.js
@@ -10,10 +10,12 @@ const {firstAidScenarioOptions, firstAidScenarioTitles, firstAidScenarios} = req
 async function displayTrivia(client, to){
 
     const questionCount = firstAidScenarioOptions.length;
-
     const questionNumber = Math.floor(Math.random() * questionCount);
 
+    const questionOptions = firstAidScenarioOptions[questionNumber];
+
     const msg = `Senaryo ${questionNumber + 1} ❓\n\n${firstAidScenarioTitles[questionNumber]}\n\n${firstAidScenarios[questionNumber]}\n\nSoru numarasını stanttakilerle paylaşırsan onlar yardımcı olurlar ⛑️`;
+    const options = `Bu senaryoda ne yapmak daha doğru??\n\n${questionOptions[0]}\n${questionOptions[1]}\n${questionOptions[2]}\n${questionOptions[3]}\n`;
 
     try {
         await client.messages.create({
@@ -21,6 +23,15 @@ async function displayTrivia(client, to){
             from: process.env.SERVICE_SID,
             to: to
         });
+
+        await client.messages.create({
+            contentSid: process.env.TRIVIA_OPTIONS,
+            contentVariables: JSON.stringify({
+                1: options
+            }),
+            from: process.env.SERVICE_SID,
+            to: to
+        })
     } catch (error) {
         console.log("Error occured in contact us stage: " + error);
     }
